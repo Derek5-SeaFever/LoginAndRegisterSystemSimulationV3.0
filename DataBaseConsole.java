efed93ddf94b2e7bd0596cbcf65898c3f88f6c52
Populus Euphratica <58065746+Derek5-SeaFever@users.noreply.github.com>
2019-11-22 17:02:49 +0800

    Create DataBaseConsole.java

---
+++ DataBaseConsole.java
@@ -0,0 +1,103 @@
+import java.util.ArrayList;
+import java.io.*;
+
+public class DataBaseConsole {
+    public void writeInName(String content) {
+        FileWriter fw = null;
+        try {
+            //如果文件存在，则追加内容；如果文件不存在，则创建文件
+            File f=new File("D:\\SeaFe\\Documents\\Java Program Files\\JavaBasicLearning\\src\\loginsystemv3\\NameData.txt");
+            fw = new FileWriter(f, true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        PrintWriter pw = new PrintWriter(fw);
+        pw.println(content);
+        pw.flush();
+        try {
+            fw.flush();
+            pw.close();
+            fw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String[] dataOfNameReader(String pathName){
+        // 使用ArrayList来存储每行读取到的字符串
+        ArrayList<String> arrayList = new ArrayList<>();
+        try {
+            File file = new File(pathName);
+            InputStreamReader inputReader = new InputStreamReader(new FileInputStream(file));
+            BufferedReader bf = new BufferedReader(inputReader);
+            // 按行读取字符串
+            String str;
+            while ((str = bf.readLine()) != null) {
+                arrayList.add(str);
+            }
+            bf.close();
+            inputReader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        // 对ArrayList中存储的字符串进行处理
+        int length = arrayList.size();
+        String[] array = new String[length];
+        for (int i = 0; i < length; i++) {
+            String s = arrayList.get(i);
+            array[i] = s;
+        }
+        return array;
+    }
+
+    public void writeInPassword(int content) {
+        FileWriter fw = null;
+        try {
+            //如果文件存在，则追加内容；如果文件不存在，则创建文件
+            File f=new File("D:\\SeaFe\\Documents\\Java Program Files\\JavaBasicLearning\\src\\loginsystemv3\\PasswordData.txt");
+            fw = new FileWriter(f, true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        PrintWriter pw = new PrintWriter(fw);
+        pw.println(content);
+        pw.flush();
+        try {
+            fw.flush();
+            pw.close();
+            fw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+
+    public int[] dataOfPasswordReader(String pathName) {
+        // 使用ArrayList来存储每行读取到的字符串
+        ArrayList<String> arrayList = new ArrayList<>();
+        try {
+            File file = new File(pathName);
+            InputStreamReader inputReader = new InputStreamReader(new FileInputStream(file));
+            BufferedReader bf = new BufferedReader(inputReader);
+            // 按行读取字符串
+            String str;
+            while ((str = bf.readLine()) != null) {
+                arrayList.add(str);
+            }
+            bf.close();
+            inputReader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        // 对ArrayList中存储的字符串进行处理
+        int length = arrayList.size();
+        int[] array = new int[length];
+        for (int i = 0; i < length; i++) {
+            String s = arrayList.get(i);
+            array[i] = Integer.parseInt(s);
+        }
+        return array;
+    }
+}
