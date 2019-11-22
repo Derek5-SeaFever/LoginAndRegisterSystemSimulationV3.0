4d0bba870592a636ed414833fda23170e572108c
Populus Euphratica <58065746+Derek5-SeaFever@users.noreply.github.com>
2019-11-22 17:01:56 +0800

    Create ErgonomicsTest.java

---
+++ ErgonomicsTest.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ErgonomicsTest {
+    public boolean ergonomicsTest(){
+        boolean successOrFailure = false;
+        int verificationCode = 1000+(int) (Math.random()*10000);
+        System.out.println("请输入以下数字来验证您不是机器："+"\n"+verificationCode);
+        Scanner input = new Scanner(System.in);
+        int codeOfUser = input.nextInt();
+        input.nextLine();
+        if(codeOfUser==verificationCode){
+            successOrFailure = true;
+        }
+        return successOrFailure;
+    }
+}
